027d3ecc7d5819d4001886cc7661210dce43ae68
Valerio <valerio.evnagelisti@prima.it>
2025-04-18 15:27:10 +0200

    feat: new banck account

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,61 @@
+use mockall::automock;
+
 fn main() {
     println!("Hello, world!");
 }
 
-
 trait AccountService {
-    fn deposit(value :i64);
-    fn withdraw(value: i64);
-    fn print_statement();
+    fn deposit(&self, value: u64);
+    fn withdraw(&self, value: u64);
+    fn print_statement(&self);
 }
 
+#[automock]
+trait Printer {
+    fn print(&self, line: &str);
+}
 
+struct Account {
+    printer: Box<dyn Printer>,
+}
+
+impl AccountService for Account {
+    fn deposit(&self, value: u64) {
+        todo!()
+    }
+
+    fn withdraw(&self, value: u64) {
+        todo!()
+    }
+
+    fn print_statement(&self) {
+        self.printer.print("Date       || Amount || Balance");
+    }
+}
 
 #[cfg(test)]
 mod tests {
+    use mockall::predicate::eq;
+
     use super::*;
 
     #[test]
-    fn it_works(){
+    fn it_works() {
         assert!(true)
     }
 
+    #[test]
+
+    fn new_bank_account_statement() {
+        let mut printer = MockPrinter::new();
+        printer
+            .expect_print()
+            .with(eq("Date       || Amount || Balance"))
+            .times(1)
+            .returning(|_| ());
+        let account = Account {
+            printer: Box::new(printer),
+        };
+        account.print_statement();
+    }
 }
